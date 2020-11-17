c0752955fcf5779c7fb9aff69c9a8f00e98f6e6c
K.T <ec2-user@ip-172-31-63-43.ec2.internal>
2020-11-17 13:07:32 +0000

    ログイン機能

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,9 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-
+  
   def index
-    @tasks = Task.all
-  end
-
-  def show
+      @tasks = current_user.tasks.order(id: :desc)
   end
 
   def new
@@ -13,18 +11,15 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      flash[:success] = 'タスクを作成しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'Task が投稿されませんでした'
+      @tasks = current_user.tasks.order(id: :desc)
+      flash.now[:danger] = 'タスクの作成に失敗しました。'
       render :new
     end
-  end
-
-  def edit
   end
 
   def update
@@ -47,9 +42,13 @@ class TasksController < ApplicationController
   private
 
   def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 
+  # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
   end
